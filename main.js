e48ce3ce1d0eb311ad6d4b9cf874d1ae412fa3ab
Nickolas Abad <nickolasabad51@gmail.com>
2023-02-02 17:43:43 -0300

    add formatacao telefone

--- main.js
+++ main.js
@@ -61,3 +61,33 @@ function atualizaTabela () {
     const corpoTabela = document.querySelector("tbody");
     corpoTabela.innerHTML = linhas;
 }
+
+const formato = {
+
+    phone (value) {
+
+    return value
+
+        .replace(/\D/g, '')
+        .replace(/(\d{2})(\d)/, '($1) $2')
+        .replace(/(\d{4})(\d)/, '$1-$2')
+        .replace(/(\d{4})-(\d)(\d{4})/, '$1$2-$3')
+        .replace(/(-\d{4})\d+?$/, '$1')
+
+    }
+
+}
+
+
+
+document.querySelectorAll('input').forEach(($input) => {
+
+    const field = $input.dataset.js
+
+    $input.addEventListener('input', (e) => {
+
+    e.target.value = formato[field](e.target.value)
+
+    }, false)
+
+})
